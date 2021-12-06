c96cd827bc93097f566408662c1afcbf83347479
yamensaad <yamen.saa199@gmail.com>
2021-12-06 21:57:31 +0100

    feat : display the text of h5 and precises

--- main.py
+++ main.py
@@ -6,7 +6,18 @@ with open('home.html', 'r') as html_file:
     content = html_file.read()
 
     soup = BeautifulSoup( content, 'lxml' )
-    print(soup.prettify())
+    course_cards = soup.findAll('div',class_= 'card')
+    for course in course_cards:
+        course_name = course.h5.text ##it will search for text attribute
+        course_price = course.a #a tage install the information about course price.
+        print(course_name)
+        print(course_price)
+        course_name = course.h5.text
+
+
+    
+
+
 
 
 
